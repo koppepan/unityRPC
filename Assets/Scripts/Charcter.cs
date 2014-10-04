2d6cad282ff68dc0f473c72b02b9bfe7cdbd9794
Horio Daichi <koppepan0114@gmail.com>
2014-10-04 13:49:12 +0900

    first commit

---
+++ Assets/Scripts/Charcter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charcter : MonoBehaviour {
+	
+	public	float	MoveSpeed;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// 自分のオブジェクトだけ操作できるようにする
+		if(true == networkView.isMine)
+		{
+			Vector3 pos = gameObject.transform.position;
+			if(Input.GetKey(KeyCode.UpArrow))
+			{
+				this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z + (MoveSpeed * Time.deltaTime));
+			}
+			else if(Input.GetKey(KeyCode.DownArrow))
+			{
+				this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z - (MoveSpeed * Time.deltaTime));
+			}
+			else if(Input.GetKey(KeyCode.RightArrow))
+			{
+				this.gameObject.transform.position = new Vector3(pos.x + (MoveSpeed * Time.deltaTime), pos.y, pos.z);
+			}
+			else if(Input.GetKey(KeyCode.LeftArrow))
+			{
+				this.gameObject.transform.position = new Vector3(pos.x - (MoveSpeed * Time.deltaTime), pos.y, pos.z);
+			}
+
+			if (Input.touchCount > 0)
+			{
+				networkView.RPC("Touch", RPCMode.All);
+			}
+
+			if (Input.GetMouseButtonDown(0))
+			{
+				networkView.RPC("Touch", RPCMode.All);
+			}
+		}
+	}
+
+	[RPC]
+	void Touch()
+	{
+		var pos = transform.position;
+		transform.position = new Vector3(pos.x, 5, pos.z);
+	}
+}
---
+++ Assets/Scripts/NetView.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetView : MonoBehaviour {
+	
+	public 		GameObject 	prefub;				// 生成したいprefab
+	
+	public 		string 		ip = "127.0.0.1";	// IPアドレス
+	private 	string 		port = "5000";		// port番号
+	
+	private 	bool 		connected = false;	// 接続されているか
+	
+	void Start()
+	{
+		// 秒間何回通信するかの設定
+		//Network.sendRate = 60;
+	}
+
+	void Update()
+	{
+	}
+		
+	
+	// purefabを実体化
+	private void CreatePlayer()
+	{
+		connected = true;
+		// 接続されているネットワーク内すべてに生成される
+		Network.Instantiate(prefub, transform.position, transform.rotation, 1);
+	}
+	
+	// サーバーとの接続が解除された(client)
+	public void OnDisconnectedFromServer()
+	{	
+		Application.Quit();
+	}
+	
+	// クライアントが切断された(host) 
+	public void OnPlayerDisconnected(NetworkPlayer pl)
+	{	
+		Network.DestroyPlayerObjects(pl);
+	}
+	
+	
+	// サーバーに接続(client)
+	public void OnConnectedToServer()
+	{
+		CreatePlayer();
+	}
+	// サーバー立ち上げ(host)
+	public void OnServerInitialized()
+	{
+		CreatePlayer();
+	}
+	
+	public void OnGUI()
+	{
+		if(!connected)
+		{
+			// IP,Port表示(入力もできるよ！)
+			ip = GUI.TextField( new Rect(10,10,90,20),ip);
+			port = GUI.TextField( new Rect(10, 40, 90, 20), port);
+			
+			// コネクトボタン、サーバーに接続
+			if( GUI.Button( new Rect( 10,70,90, 20), "Connect"))
+			{
+				Network.Connect(ip, int.Parse(port) );
+			}
+			// ホストボタン、サーバー立ち上げ
+			if( GUI.Button( new Rect(10, 100, 90, 20), "host"))
+			{	
+				Network.InitializeServer(10, int.Parse(port), true);
+			}
+		}
+	}
+}
